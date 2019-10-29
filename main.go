442a205104b0c3ebe37b15651b1e39f2ab0bc207
Paulo Gomes <paulo.gomes.uk@gmail.com>
2019-10-29 11:58:07 +0000

    Add tests for command line

--- main.go
+++ main.go
@@ -8,7 +8,8 @@ import (
 )
 
 var (
-	usage string = `gosystract returns the names and IDs of all system calls being called inside a go application.
+	noSyscallsFoundMessage string = "no syscalls found in %s\n"
+	usageMessage           string = `gosystract returns the names and IDs of all system calls being called inside a go application.
 Usage: 
 	gosystrac goapp.dump
 
@@ -18,7 +19,7 @@ To generate a dump file from a go application use:
 
 func main() {
 	if len(os.Args) != 2 {
-		fmt.Println(usage)
+		fmt.Println(usageMessage)
 		os.Exit(1)
 	}
 
@@ -29,7 +30,7 @@ func main() {
 	}
 
 	if len(syscalls) == 0 {
-		fmt.Printf("no syscalls found in %s\n", fileName)
+		fmt.Printf(noSyscallsFoundMessage, fileName)
 	} else {
 		fmt.Printf("%d syscalls found:\n\n", len(syscalls))
 		for _, syscall := range syscalls {
---
+++ main_test.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"fmt"
+	"os/exec"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestUsage_NoArgs(t *testing.T) {
+	assert := assert.New(t)
+
+	cmd := exec.Command("go",
+		"run",
+		"main.go")
+
+	output, err := cmd.CombinedOutput()
+
+	actual := string(output)
+
+	assert.Error(err, "when no args provided should error")
+	assert.Contains(
+		actual,
+		usageMessage,
+		"when no args provided should show usage")
+}
+
+func TestMain_NoSyscalls(t *testing.T) {
+	assert := assert.New(t)
+
+	fileName := "test/no-syscalls.dump"
+	cmd := exec.Command("go",
+		"run",
+		"main.go",
+		fileName)
+
+	output, err := cmd.Output()
+
+	actual := string(output)
+	expected := fmt.Sprintf(noSyscallsFoundMessage, fileName)
+
+	assert.Nil(err)
+	assert.Contains(actual, expected)
+}
+
+func TestMain(t *testing.T) {
+	assert := assert.New(t)
+
+	fileName := "test/single-syscall.dump"
+	cmd := exec.Command("go",
+		"run",
+		"main.go",
+		fileName)
+
+	output, err := cmd.Output()
+
+	actual := string(output)
+	expected := "1 syscalls found:\n\nexit_group (231)"
+
+	assert.Nil(err)
+	assert.Contains(actual, expected)
+}
