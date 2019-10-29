1be83e97606b1f8eaf8b5368c23815f478826237
Paulo Gomes <paulo.gomes.uk@gmail.com>
2019-10-29 10:57:03 +0000

    Force tmp folder to be created

--- .github/tools/run-gosec.sh
+++ .github/tools/run-gosec.sh
@@ -2,10 +2,11 @@
 set -e
 set -o pipefail
 
-wget https://github.com/securego/gosec/releases/download/2.0.0/$GOSECNAME.tar.gz -O "/tmp/$GOSECNAME.tar.gz"
+wget https://github.com/securego/gosec/releases/download/2.0.0/$GOSECNAME.tar.gz -O /tmp/$GOSECNAME.tar.gz
 echo "490c2a0434b2b9cbb2f4c5031eafe228023f1ac41b36dddd757bff9e1de76a2b /tmp/$GOSECNAME.tar.gz" | sha256sum -c -
 
-tar -xzf /tmp/$GOSECNAME.tar.gz
+mkdir -p /tmp/$GOSECNAME
+tar -C /tmp/$GOSECNAME -xzf /tmp/$GOSECNAME.tar.gz
 
 /tmp/$GOSECNAME/gosec -conf gosec.json ./...
 
